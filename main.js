e5e894a50daac5d0bffd6485cea7a8e3f924d00d
Vidit Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2023-09-03 13:56:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function writer() {
+    window.alert("Letter Written by- VIDIT KESHARI 7 Iris");
+}
+
+function wteacherdaymade() {
+    window.alert("Page loaded");
+    window.location = "teacher.html";
+}
+
+function home() {
+    window.alert("Page loaded");
+    window.location = "index.html";
+}
+
+function wsr() {
+    window.alert("Page loaded");
+    window.location = "wsr.html";
+}
+
+function whatadicates() {
+    window.alert("Page loaded");
+    window.location = "whatadicates.html";
+}
+
+function Home() {
+    window.alert("<Page loaded>?//:home.open")
+    window.location = "index.html";
+}
